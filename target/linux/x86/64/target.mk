a0dff860dafdf3112cd0ff467aa4c30b5b792f6b
hungleungi <hungleung.me@icloud.com>
2020-12-22 23:10:04 +0800

    Update target.mk

--- target/linux/x86/64/target.mk
+++ target/linux/x86/64/target.mk
@@ -1,6 +1,6 @@
 ARCH:=x86_64
 BOARDNAME:=x86_64
-DEFAULT_PACKAGES += kmod-button-hotplug kmod-vmxnet3 acme-dnsapi luci-app-acme luci-app-ddns luci-app-wireguard luci-app-udpxy luci-app-wol luci-app-vlmcsd luci-app-passwall
+DEFAULT_PACKAGES += kmod-button-hotplug kmod-vmxnet3 acme-dnsapi luci-app-acme luci-app-ddns luci-app-wireguard luci-app-udpxy luci-app-wol luci-app-vlmcsd luci-app-passwall luci-app-unpn
 
 define Target/Description
         Build images for 64 bit systems including virtualized guests.
